d76d088020e4eb72375bec30dc7c9abd1cc3f0b9
Arslan Mushtaq <m.arslanmushtaqahmed@gmail.com>
2020-01-31 23:18:05 +0500

    #EJPL03(6) - Integer

---
+++ Integer/IntegerSize.java
@@ -0,0 +1,16 @@
+class IntegerSize {
+
+    public static void main(String[] args) {
+        
+        int max = Integer.MAX_VALUE;
+        System.out.println("Max value of Integer: " + max);
+
+        int min = Integer.MIN_VALUE;
+        System.out.println("Min value of Integer: " + min);
+
+        int size = Integer.SIZE;
+        System.out.println("Size of Integer: " + size);
+
+    }
+    
+}
