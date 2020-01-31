f5ba9b5256ecd6ab1b8cbd1fa789009857859641
Arslan Mushtaq <m.arslanmushtaqahmed@gmail.com>
2020-01-31 23:39:35 +0500

    #EJPL03(7) - Long

---
+++ Long/LongSize.java
@@ -0,0 +1,16 @@
+class LongSize {
+
+    public static void main(String[] args) {
+        
+        long max = Long.MAX_VALUE;
+        System.out.println("Max value of Long: " + max);
+
+        long min = Long.MIN_VALUE;
+        System.out.println("Min value of Long: " + min);
+
+        long size = Long.SIZE;
+        System.out.println("Size of long: " + size);
+
+    }
+    
+}
