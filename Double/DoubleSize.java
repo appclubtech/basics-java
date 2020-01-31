2fa3b84693afa290ae58f30b67588ce6c393d100
Arslan Mushtaq <m.arslanmushtaqahmed@gmail.com>
2020-02-01 01:17:20 +0500

    #EJPL03(9) - Double

---
+++ Double/DoubleSize.java
@@ -0,0 +1,19 @@
+class DoubleSize {
+
+    public static void main(String[] args) {
+        
+        double number = 100.50; // 100.50D
+        System.out.println("Number: " + number);
+
+        double max = Double.MAX_VALUE;
+        System.out.println("Max value of Double: " + max);
+
+        double min = Double.MIN_VALUE;
+        System.out.println("Min value of Double: " + min);
+
+        double size = Double.SIZE;
+        System.out.println("Size of Double: " + size);
+
+    }
+    
+}
