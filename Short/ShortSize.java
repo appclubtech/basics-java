51fdcd10bb2e5055dc6d169d7d796434ee008462
Arslan Mushtaq <m.arslanmushtaqahmed@gmail.com>
2020-01-31 22:06:08 +0500

    #EJPL03(5) - Short

---
+++ Short/ShortSize.java
@@ -0,0 +1,12 @@
+class ShortSize {
+
+    public static void main(String[] args) {
+        
+        short max = Short.MAX_VALUE;
+        System.out.println("Max Value of Short: " + max);
+
+        short min = Short.MIN_VALUE;
+        System.out.println("Min Value of Short: " + min);
+        
+    }
+}
