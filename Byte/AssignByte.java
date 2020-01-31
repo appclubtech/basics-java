7802fa15884a965df38deb5d1dbf4fed9fa83888
Arslan Mushtaq <m.arslanmushtaqahmed@gmail.com>
2020-01-31 22:00:45 +0500

    #EJPL03(4) - Byte

---
+++ Byte/AssignByte.java
@@ -0,0 +1,10 @@
+class AssignByte {
+
+    public static void main(String[] args) {
+        
+        byte number = 100;
+        System.out.println(number + " bytes");
+
+    }
+    
+}
---
+++ Byte/ByteSize.java
@@ -0,0 +1,12 @@
+class ByteSize {
+
+    public static void main(String[] args) {
+        
+        byte max = Byte.MAX_VALUE;
+        System.out.println("Max value of Byte: " + max);
+
+        byte min = Byte.MIN_VALUE;
+        System.out.println("Min value of Byte: " + min);
+
+    }
+}
