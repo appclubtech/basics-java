25f8d8a7ab4ffb20c882c1144b0cda164dc716d2
Arslan Mushtaq <m.arslanmushtaqahmed@gmail.com>
2020-02-01 00:04:10 +0500

    #EJPL03(8) - Float

---
+++ Float/Difference.java
@@ -0,0 +1,15 @@
+class Difference {
+
+    public static void main(String[] args) {
+        
+        int intValueOne = 1;
+        int intValueTwo = 2;
+        System.out.println("Integer Result: " + (intValueOne / intValueTwo));
+
+        float floatValueOne = 1.0f;
+        float floatValueTwo = 2.0f;
+        System.out.println("Float Result: " + (floatValueOne / floatValueTwo));
+
+    }
+    
+}
---
+++ Float/FloatSize.java
@@ -0,0 +1,18 @@
+class FloatSize {
+
+    public static void main(String[] args) {
+
+        float number = 9.0f;
+        System.out.println("Number: " + number);
+
+        float max = Float.MAX_VALUE;
+        System.out.println("Max value of Float: " + max);
+
+        float min = Float.MIN_VALUE;
+        System.out.println("Min value of Float: " + min);
+
+        float size = Float.SIZE;
+        System.out.println("Size of Float: " + size);
+    }
+
+}
