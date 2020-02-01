a16e11c9d439c825678b1d06cf592374ee366930
Arslan Mushtaq <m.arslanmushtaqahmed@gmail.com>
2020-02-02 03:42:39 +0500

    #EJPL05(2) - While Loop

---
+++ While Loop/WhileLoop.java
@@ -0,0 +1,14 @@
+class WhileLoop {
+
+    public static void main(String[] args) {
+        
+        float count = 0.0f;
+
+        while (count <= 10.0f) {
+            System.out.print(count + " ");
+            count++;
+        }
+
+    }
+    
+}
