f30b6433a20484116ebf30ee2b34d7f2bfab713c
Arslan Mushtaq <m.arslanmushtaqahmed@gmail.com>
2020-02-02 04:06:38 +0500

    #EJPL05(4) - For Each Loop

---
+++ For Each Loop/ForEachLoop.java
@@ -0,0 +1,12 @@
+class ForEachLoop {
+
+    public static void main(String[] args) {
+
+        String[] values = { "This", "is", "Java", "for", "Beginners" };
+
+        for (String value : values) {
+            System.out.print(value + " ");
+        }
+        
+    }
+}
