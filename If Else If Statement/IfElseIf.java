1ba941e2a56b7677fd347179d5bfdf19e9bf525c
Arslan Mushtaq <m.arslanmushtaqahmed@gmail.com>
2020-02-02 04:24:14 +0500

    #EJPL06(2) - If Else If Statement

---
+++ If Else If Statement/IfElseIf.java
@@ -0,0 +1,14 @@
+class IfElseIf {
+
+    public static void main(String[] args) {
+
+        int number = 100;
+
+        if (number < 55) {
+            System.out.println("Number is less than 55.");
+        } else {
+            System.out.println("Number is greater than 55.");
+        }
+        
+    }
+}
