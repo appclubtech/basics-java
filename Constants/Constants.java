79157b324ec65f1a609973900262453e43b63c6e
Arslan Mushtaq <m.arslanmushtaqahmed@gmail.com>
2020-02-01 19:39:51 +0500

    #EJPL04(2) - Constants

---
+++ Constants/Constants.java
@@ -0,0 +1,11 @@
+class Constants {
+
+    static final String PROGRAMMING_LANGUAGE = "Java";
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, " + PROGRAMMING_LANGUAGE);
+        
+    }
+    
+}
