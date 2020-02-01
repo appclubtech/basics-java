b6e85ab3fc99e711411a02b5ff059308b01c5094
Arslan Mushtaq <m.arslanmushtaqahmed@gmail.com>
2020-02-02 03:47:28 +0500

    #EJPL05(3) - Do While Loop

---
+++ Do While Loop/DoWhileLoop.java
@@ -0,0 +1,13 @@
+class DoWhileLoop {
+
+    public static void main(String[] args) {
+        
+        int count = 0;
+        do {
+            System.out.print(count + " ");
+            count++;
+        } while (count < 10);
+
+    }
+    
+}
