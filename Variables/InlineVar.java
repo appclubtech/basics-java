3fc479e593ad0c308032e91b51e489a2261bcaf1
Arslan Mushtaq <m.arslanmushtaqahmed@gmail.com>
2020-02-02 17:22:25 +0500

    Adding Inline Variables

---
+++ Variables/InlineVar.java
@@ -0,0 +1,13 @@
+class InlineVar {
+
+    public static void main(String[] args) {
+        
+        int a = 1, b = 2, c = 3;
+        
+        System.out.println("a is " + a);
+        System.out.println("b is " + b);
+        System.out.println("c is " + c);
+
+    }
+    
+}
