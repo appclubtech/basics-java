f96d2f165eb4f55f7881015bd41ac846517b4268
Arslan Mushtaq <m.arslanmushtaqahmed@gmail.com>
2020-02-02 14:37:55 +0500

    #EJPL06(5) - Ternary Operator

---
+++ Ternary Operator/Ternary.java
@@ -0,0 +1,14 @@
+class Ternary {
+
+    public static void main(String[] args) {
+        
+        String dinner = "Chicken Burger";
+
+        String decision = dinner.contains("Burger") ? "We are having burgers" : "Oh! No burgers today?";
+
+        System.out.println(decision);
+
+
+    }
+    
+}
