3396a496be28f9a0fbb455da560f173de3b32b6c
Arslan Mushtaq <m.arslanmushtaqahmed@gmail.com>
2020-02-02 17:28:48 +0500

    Adding Comments

---
+++ Comments/MultiLine.java
@@ -0,0 +1,12 @@
+class MultiLine {
+
+    public static void main(String[] args) {
+        
+        /**
+        * This is a Multi-Line Comment
+        */
+
+    }
+
+    
+}
---
+++ Comments/SingleLine.java
@@ -0,0 +1,9 @@
+class SingleLine {
+
+    public static void main(String[] args) {
+        
+        // This is a single line comment
+
+    }
+    
+}
